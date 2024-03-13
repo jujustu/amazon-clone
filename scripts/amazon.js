b9037f0f3150a425b4c6c7152093266ad55d96d1
Herobrine <prayashsingh608@gmail.com>
2024-03-13 21:07:39 +0530

    move addtocart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
-import {cart} from'../data/cart.js';
+import {cart,addtocart} from'../data/cart.js';
 import {products} from'../data/products.js';
+
 
 
 let productsHTML = "";
@@ -58,35 +59,25 @@ products.forEach((product) => {
   `;
 });
 
+function updatecart(){
+  let cartQuantity = 0;
+
+  cart.forEach((item) =>{
+      cartQuantity += item.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+
 document.querySelector(".js-product-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
-    let matchingItem;
+    addtocart(productId);
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) =>{
-        cartQuantity += item.quantity;
-    })
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    updatecart();
 
   });
 });
