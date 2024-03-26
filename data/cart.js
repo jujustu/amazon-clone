0024771b88d0ccb9ee69ee6ce2e18fb080952fcd
Herobrine <prayashsingh608@gmail.com>
2024-03-26 21:22:38 +0530

    gave a default value to cart array

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export let cart = JSON.parse(localStorage.getItem('cart',)) || [];  
+export let cart = JSON.parse(localStorage.getItem('cart',)) || [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 1,
+    deliveryId: '1'
+  },{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+    deliveryId:'2',
+  }
+];  
 
 
 function saveToStorage(){
@@ -22,7 +32,7 @@ export function addtocart(productId){
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: 1, 
       });
     }
     saveToStorage();
